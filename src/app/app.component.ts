724aaad95d1666432696c4793ae1d8e3a44fd769
aayush kc <kcaayush2158@gmail.com>
2019-06-07 17:47:15 +0545

    Projet uploaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'tdf';
   topics =['java','javascript'];
+  userModel = new User('rob','rob@gmail.com',23293,' sdasd','morning',true);
+
 }
